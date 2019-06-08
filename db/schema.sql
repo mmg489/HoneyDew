22fb73b16952c4bddb6d5ef2044700d7f7e0ff0b
aupreti1 <anaupreti@gmail.com>
2019-06-08 12:46:04 -0400

    added the connection and edited the seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,31 @@
 CREATE DATABASE honeydew_db;
 USE honeydew_db;
 
-TABLE1
+-- TABLE1
 
 CREATE TABLE users (
-    userid int NOT NULL AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     acct_name VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (userid)
+    secret_word VARCHAR(255) NOT NULL,
+    userone_name VARCHAR(255) NOT NULL,
+    usertwo_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-TABLE2
+-- TABLE2
 
 CREATE TABLE foods (
-    foodid int NOT NULL AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     meal_name VARCHAR(255) NOT NULL,
-    swipe int,
-    PRIMARY KEY (foodid),
+    swipe int NOT NULL DEFAULT 0,
+    PRIMARY KEY (id),
 );
 
-TABLE3
+-- TABLE3
 
 CREATE TABLE activities (
-    activities_id int NOT NULL AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     event_name VARCHAR(255) NOT NULL,
-    swipe int,
-    PRIMARY KEY (activities_id),
+    swipe int NOT NULL DEFAULT 0,
+    PRIMARY KEY (id),
 );
