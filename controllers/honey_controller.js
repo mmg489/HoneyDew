abbe37c1e70c973fbb662502e5ca302fa8754eb1
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-06-08 12:59:15 -0400

    controller WIP

--- controllers/honey_controller.js
+++ controllers/honey_controller.js
@@ -2,10 +2,32 @@
 var express = require('express');
 var router = express.Router();
 
+// imports foods and activities objects with methods
+var foods = require('../models/foods.js');
+var activities = require('../models/activities.js');
+
 // basic homepage route
 router.get('/', function (req, res) {
     res.render('index');
-})
+});
+
+router.get('/dashboard', function (req, res) {
+    res.render('dashboard');
+});
+
+router.get('/api/like/foods', function (req, res) {
+    foods.all(function(data) {
+        res.render('swipe', {foods_data: data})
+    })
+});
+
+router.get('/api/like/activities', function (req, res) {
+    activities.all(function(data) {
+        res.render('swipe', {activites_data: data})
+    })
+});
+
+router.post('/api/add')
 
 // exports express router
 module.exports = router;
