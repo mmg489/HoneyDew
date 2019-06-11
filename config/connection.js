e8b5529732755008c6d3bff972c580f4ecf5e923
Margarita <m_mg489@outlook.com>
2019-06-10 20:17:53 -0400

    changed the connection back to process.env format

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,10 @@ if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL)}
     else {
     connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: process.env.DB_HOST,
+    port:3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: 'honeydew_db'
 })
 };
