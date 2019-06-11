1ea8633b723884d53818dcb5f177ad08a2745cbf
aupreti1 <anaupreti@gmail.com>
2019-06-10 20:49:01 -0400

    changed the indentation of connection

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,20 @@ require('dotenv').config();
 
 var connection;
 
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL)}
-    else {
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
     connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'honeydew_db'
-})
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'honeydew_db'
+    })
 };
-connection.connect(function(err){
-	if(err)throw err;
-	console.log("connected as id" + connection.threadId);
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId);
 });
 
 module.exports = connection;
