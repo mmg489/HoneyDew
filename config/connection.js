d7f8a6647c02b7d5e39e77e1e78c0d3c839df5fb
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-06-13 09:21:25 -0400

    conflict resolved

--- config/connection.js
+++ config/connection.js
@@ -5,19 +5,6 @@ require('dotenv').config();
 var connection;
 
 if (process.env.JAWSDB_URL) {
-<<<<<<< HEAD
-	connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-
-
-	connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	port:3306,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: "honeydew_db"
-	})
-=======
     connection = mysql.createConnection(process.env.JAWSDB_URL)
 }
 else {
@@ -29,7 +16,6 @@ else {
     password: process.env.DB_PASS,
     database: 'honeydew_db'
 })
->>>>>>> 1a6b78b2ec704e62989b9cbdd0ab793fe761bbb6
 };
 connection.connect(function (err) {
     if (err) throw err;
