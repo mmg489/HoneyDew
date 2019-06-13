929816951aab741241b7a855e6e7d5807ca8fee0
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-06-13 11:57:37 -0400

    more bug fixes

--- config/orm.js
+++ config/orm.js
@@ -80,7 +80,7 @@ var orm = {
         var queryString = "UPDATE " + table;
 
         queryString += " SET ";
-        queryString += objToSql(objColVals);
+        queryString += objColVals;
         queryString += " WHERE ";
         queryString += condition;
 
