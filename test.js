dbf083ffaa08d599b1439889dd895016b363158a
aupreti1 <anaupreti@gmail.com>
2019-06-15 09:33:30 -0400

    still working on testing

--- test.js
+++ test.js
@@ -3,38 +3,38 @@ var input = require("../server");
 
 describe("Input", function() {
   it("to verify if username is blank and if password is blank", function() {
-    expect(input(" ")).to.equal(406);
+    expect(input(" ", " ")).to.equal(406);
   });
 
   it("to verify if username is blank but password is invalid", function() {
-    expect(input(" ")).to.equal(406);
+    expect(input(" ", "Invalid")).to.equal(406);
   });
 
   it("to verify if usernme is blank but password is valid", function() {
-    expect(input(" ")).to.equal(406);
+    expect(input(" ", "Valid")).to.equal(406);
   });
 
   it("to verify if username is invalid but password is blank", function() {
-    expect(input(" ")).to.equal(406);
+    expect(input("Invalid", " ")).to.equal(406);
   });
 
   it("to verify is username is valid but password is blank", function() {
-    expect(input(" ")).to.equal(406);
+    expect(input("Valid", " ")).to.equal(406);
   });
 
   it("to verify if username is invalid and password is invalid", function() {
-    expect(input(" ")).to.equal(406);
+    expect(input("Invalid", "Invalid")).to.equal(406);
   });
 
   it("to verify if username is invalid and password is valid", function() {
-    expect(input(" ")).to.equal(406);
+    expect(input("Invalid", "Valid")).to.equal(406);
   });
 
   it("to verify if username is valid and password is invalid", function() {
-    expect(input(" ")).to.equal(406);
+    expect(input("Valid", "Invalid")).to.equal(406);
   });
 
   it("to verify if usernme is valid and password in valid", function() {
-    expect(input(" ")).to.equal(200);
+    expect(input("Valid", "Valid")).to.equal(200);
   });
 });
