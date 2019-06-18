78a0b6d4f0e83930d83f86e7028f38de3f4afa40
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-06-18 11:41:30 -0400

    deploy

--- config/connection.js
+++ config/connection.js
@@ -4,17 +4,14 @@ require('dotenv').config();
 
 var connection;
 
-if (process.env.JAWSDB_URL) {
-    // Database is JawsDB on Heroku
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else if (process.env.AWS_HOST) {
-//     connection = mysql.createConnection({
-//         host: process.env.AWS_HOST,
-//         port: 3306,
-//         user: process.env.AWS_USER,
-//         password: process.env.AWS_PASS,
-//         database: 'honeydew'
-//     })
+if (process.env.AWS_HOST) {
+    connection = mysql.createConnection({
+        host: process.env.AWS_HOST,
+        port: 3306,
+        user: process.env.AWS_USER,
+        password: process.env.AWS_PASS,
+        database: 'honeydew'
+    })
  } else {
      connection = mysql.createConnection({
          host: process.env.DB_HOST,
