3b25585751bd69638981f984def3b60e8c9e8432
Gigi Tannous <gtannous16@gmail.com>
2019-06-17 23:38:19 -0400

    I think we're done yay

--- models/foods.js
+++ models/foods.js
@@ -8,31 +8,16 @@ var foods = {
       cb(res);
     });
   },
-<<<<<<< HEAD
-  //create: function(mealidea, cb) {
-  //  orm.create('foods', 'meal_name', mealidea, cb);
-  //},
-
-  delete: function(id, cb) {
-=======
   create: function (mealidea, cb) {
     orm.create('foods', 'meal_name', mealidea, cb);
   },
-  update: function (id, cb) {
-    var condition = "id=" + id;
-    orm.update('foods', 'swipe = swipe + 1', condition, cb);
-  },
-  undo: function (id, cb) {
-    var condition = "id=" + id;
-    orm.update('foods', 'swipe = swipe - 1', condition, cb);
-  },
+
   both: function (col, uniqueurl, cb) {
     orm.both('user_likes', col, uniqueurl, function (res) {
       cb(res);
     });
   },
   delete: function (id, cb) {
->>>>>>> ee2e05fcbfcb7b64bc341815bb72c5e10f3092ea
     var condition = "id=" + id;
     orm.delete('foods', condition, function (res) {
       cb(res);
